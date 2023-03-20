21355b8edd19c59d4c0db5a5b47e90046968494e
Simon Stockton <61173796+simonstockton763@users.noreply.github.com>
2023-03-20 18:28:49 -0500

    Documentation
    Added documentation for better clarity

--- ContactExtract.py
+++ ContactExtract.py
@@ -6,18 +6,22 @@ imap_server =''
 imap_user = ''
 imap_password = ''
 
+#Connects to given IMAP serrver, then logs in with given credentials
 conn = imaplib.IMAP4_SSL(imap_server)
 (retcode, capabilities) = conn.login(imap_user, imap_password)
 
-conn.list()
+#Debug line to find correct inbox folder
+#conn.list()
 
 conn.select("Inbox", readonly=True)
 result, data = conn.uid('search', None, 'ALL')
 uids = data[0].split()
 
+#Fetches the FROM section of the header section of all emails in folder
 result, data = conn.uid('fetch',  b','.join(uids),'(BODY[HEADER.FIELDS (FROM)])')
 raw_file = open('raw-email-rec.tsv', 'w')
 
+#Writes addresses to file 
 for i in range(0, len(data)):
      
     if len(data[i]) != 2:
